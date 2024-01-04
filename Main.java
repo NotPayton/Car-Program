f3812ebc217b576ebf0ab3466a524d5c9242fc51
Payton <144721332+NotPayton@users.noreply.github.com>
2024-01-04 13:21:22 -0500

    haiiiii :3

--- Main.java
+++ Main.java
@@ -4,3 +4,11 @@ public class Main {
         System.out.println(Payton);
     }
 }
+
+/*public class Main {
+   public static void main(String args[]) {
+       Car car1 = new Car();
+       System.out.print(car1.carfax());
+   }
+}
+*/
